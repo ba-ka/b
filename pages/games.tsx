d09d06b2749c689e8d7c2ecdd57243befae16fc2
datoyacx <datoyacx@outlook.jp>
2022-03-12 03:56:34 +0700

    add list data

--- pages/games.tsx
+++ pages/games.tsx
@@ -2,22 +2,27 @@ import type { NextPage } from 'next'
 import Layout from '../components/layout'
 
 const Games: NextPage = () => {
+  const gamesList = [
+    { title: "GHOST ID Project", url: "https://www.roblox.com/games/5253868577/gip", description: "Game about fighting the ghost using weapon. This game is inspired by zombie game in Roblox." }
+  ]
   return (
     <Layout title="games">
       <div className="sm:container sm:mx-auto pt-8 sm:pt-12 px-6 sm:px-0">
-        <figure className="bg-gray-50 border border-gray-900 mb-6">
-          <div className="p-6 md:p-8 text-left space-y-4">
-            <h2 className="text-4xl">GHOST ID Project</h2>
-            <blockquote>
-              <p className="text-lg">
-                Game about fighting the ghost using weapon. This game is inspired by zombie game in Roblox.
-              </p>
-            </blockquote>
-            <a target="_blank" href="https://ba-ka.org/gip" className="inline-block mt-3 mb-6 sm:mb-0 text-lg border border-gray-900 py-2 px-6 bg-transparent text-gray-900 hover:bg-gray-900 hover:text-gray-50 duration-200">
-              <span className="text-md sm:text-lg duration-200">play</span>
-            </a>
-          </div>
-        </figure>
+        {gamesList?.map((item) => (
+          <figure className="bg-gray-50 border border-gray-900 mb-6">
+            <div className="p-6 md:p-8 text-left space-y-4">
+              <h2 className="text-4xl">{item.title}</h2>
+              <blockquote>
+                <p className="text-lg">
+                  {item.description}
+                </p>
+              </blockquote>
+              <a target="_blank" href={item.url} className="inline-block mt-3 mb-6 sm:mb-0 text-lg border border-gray-900 py-2 px-6 bg-transparent text-gray-900 hover:bg-gray-900 hover:text-gray-50 duration-200">
+                <span className="text-md sm:text-lg duration-200">play</span>
+              </a>
+            </div>
+          </figure>
+        ))}
       </div>
     </Layout>
   )
--- pages/works.tsx
+++ pages/works.tsx
@@ -2,12 +2,31 @@ import { NextPage } from "next"
 import Layout from "../components/layout"
 
 const Works: NextPage = () => {
+  const worksList = [
+    { title: "empugaris.com", url: "https://empugaris.com/", description: "web slicing" },
+    { title: "sehatcruises.com", url: "https://sehatcruises.com/", description: "web slicing" },
+    { title: "duplen.id", url: "https://duplen.id/", description: "web slicing and design" },
+    { title: "jaktel.com", url: "https://jaktel.com/", description: "web slicing" },
+    { title: "phmi.co.id", url: "https://phmi.co.id/", description: "web slicing" }
+  ]
   return (
     <Layout title="works">
-      <div className="sm:container sm:mx-auto px-6 sm:px-0">
-        <div className="text-center pt-4">
-          coming soon.
-        </div>
+      <div className="sm:container sm:mx-auto pt-8 sm:pt-12 px-6 sm:px-0">
+        {worksList?.map((item) => (
+          <figure className="bg-gray-50 border border-gray-900 mb-6">
+            <div className="p-6 md:p-8 text-left space-y-4">
+              <h2 className="text-4xl">{item.title}</h2>
+              <blockquote>
+                <p className="text-lg">
+                  {item.description}
+                </p>
+              </blockquote>
+              <a target="_blank" href={item.url} className="inline-block mt-3 mb-6 sm:mb-0 text-lg border border-gray-900 py-2 px-6 bg-transparent text-gray-900 hover:bg-gray-900 hover:text-gray-50 duration-200">
+                <span className="text-md sm:text-lg duration-200">view</span>
+              </a>
+            </div>
+          </figure>
+          ))}
       </div>
     </Layout>
   )
