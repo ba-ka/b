dfd8f3860cd778ee490f644fef31f65b59110efc
datoyacx <datoyacx@outlook.jp>
2022-03-16 22:10:04 +0700

    add email

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -7,7 +7,8 @@ const Contact: NextPage = () => {
       <div className="sm:container sm:mx-auto pt-8 sm:pt-12 px-6 sm:px-0">
         other than in the list are fake
         <ul className="list-disc pl-6">
-            <li><a target="_blank" rel="noopener noreferrer" href="https://ba-ka.org/discord">discord (server invite)</a> / datoyacx#5006 (personal)</li>
+            <li><a target="_blank" rel="noopener noreferrer" href="https://ba-ka.org/discord">discord (server invite)</a> / datoyacx#5006 (toya)</li>
+            <li><a target="_blank" rel="noopener noreferrer" href="mailto:tx@ba-ka.org">e-mail</a> / tx@ba-ka.org (tholut x)</li>
             <li><a target="_blank" rel="noopener noreferrer" href="https://ba-ka.org/github">github</a></li>
             <li><a target="_blank" rel="noopener noreferrer" href="https://www.roblox.com/groups/6503101/bakahu">roblox</a></li>
             <li><a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UCvFbESDODki3LoaeVlqNv4A">youtube</a></li>
