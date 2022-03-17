dbe567c4f7452c6608aedde7ea51652153be29d6
datoyacx <datoyacx@outlook.jp>
2022-03-18 01:00:44 +0700

    add maze games

--- pages/games.tsx
+++ pages/games.tsx
@@ -3,7 +3,8 @@ import Layout from '../components/layout'
 
 const Games: NextPage = () => {
   const gamesList = [
-    { title: "GHOST ID Project", url: "https://www.roblox.com/games/5253868577/gip", description: "Game about fighting the ghost using weapon. This game is inspired by zombie game in Roblox." }
+    { title: "GHOST ID Project", url: "https://www.roblox.com/games/5253868577/gip", description: "Game about fighting the ghost using weapon. This game is inspired by zombie game in Roblox." },
+    { title: "Maze Games [closed]", url: "https://www.roblox.com/games/5111122533/mg", description: "Minigame but you are in Maze location and It require 2 Players or more to play. There are murderer who will kill innocent or police will kill murderer in Maze." }
   ]
   return (
     <Layout title="games">
